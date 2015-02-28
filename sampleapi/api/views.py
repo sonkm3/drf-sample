7dd657a779c0b9096db799c0c58e70ad964e7eee
sonkm3 <sonkm3@gmail.com>
2015-02-28 11:58:10 +0900

    JSONを取得するだけのAPIを作る > $ curl http://localhost:8000/api/temp/ > {"single-int": 1, "dict": {"key1": "val1", "key2": "val2"}, "list": [1, 2, 3]}

--- sampleapi/api/views.py
+++ sampleapi/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.response import Response
+from rest_framework import viewsets
+
+class TempViewSet(viewsets.ViewSet):
+    def list(self, request):
+        data = {}
+        data['single-int'] = 1
+        data['list'] = [1,2,3]
+        data['dict'] = {'key1': 'val1','key2': 'val2',}
+        return Response(data)
+
+
--- sampleapi/sampleapi/urls.py
+++ sampleapi/sampleapi/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from rest_framework.routers import DefaultRouter
+from api import views
+
+api_router = DefaultRouter()
+api_router.register(r'temp', views.TempViewSet, base_name = 'temp_view')
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sampleapi.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^api/', include(api_router.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
 )
